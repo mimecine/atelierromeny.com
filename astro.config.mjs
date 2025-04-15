6f9379166c97f8f0738574cef16f70406498a081
Marco Romeny <marco@mimecom.net>
2025-04-15 14:13:57 +0100

    Absolute -> relative url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atelierromeny.com',
+  site: 'https://atelierromeny.com/',
   vite: {
     plugins: [tailwindcss()]
   }
